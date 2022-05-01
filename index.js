47a4be780c51c4d4a5413abdbf39b4f8213453aa
gwenaelgressier <97903290+gwenaelgressier@users.noreply.github.com>
2022-05-02 00:43:42 +0200

    mise en place de cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-console.log("Hello World");
-console.log("Hello World");
-
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
 
+//Middleware
+app.use(cors()); //Le CORS permet de prendre en charge des requêtes multi-origines sécurisées et des transferts de données entre le navigateurs et le serveurs web
+
+//Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
